43ab3f8b5ec1f5845bb179793a6a26142c34f1f0
Iryna Kosiakovska <ira.oleinick2013@gmail.com>
2024-05-21 14:16:24 +0200

    updated

--- IrynaKosiakovska/Top1Sum.sql
+++ IrynaKosiakovska/Top1Sum.sql
@@ -1,5 +1,7 @@
-SELECT currency, sum(amount) as total_sum FROM txs
-join accounts on txs.sender_account_id = accounts.id
+SELECT currency,
+sum(amount) as total_sum
+FROM transact
+join accounts on transact.sender_account_id = accounts.id
 group by currency
 order by total_sum DESC
 limit 1
